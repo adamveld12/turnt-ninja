6b4bc48bff7552072a1cde437995419718a289b2
Adam Veldhousen <adamveld12@gmail.com>
2015-02-23 11:27:09 -0600

    added executable lines to shell scripts

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 hugo --source=$GOPATH/src/github.com/adamveld12/turnt-ninja --theme="hughost" --destination=$GOPATH/src/github.com/adamveld12/turnt-ninja/public
--- dev.sh
+++ dev.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 hugo server --watch -D --source=$GOPATH/src/github.com/adamveld12/turnt-ninja --theme="hughost" --destination=$GOPATH/src/github.com/adamveld12/turnt-ninja/public -F
